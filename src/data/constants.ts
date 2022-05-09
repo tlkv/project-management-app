9b82c9a1c313aab577c38b5ca434a4a740359b61
deftonjke <defton@mail.ru>
2022-05-10 02:15:15 +0400

    feat: add constants

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -75,3 +75,12 @@ export const SEARCH_INFO_DEFAULT: SearchInfo = {
 };
 
 export const API_URL = 'https://rs-team-34.herokuapp.com';
+
+export const CONFIRM_MODAL_DEFAULT = {
+  question: '',
+  isOpen: false,
+  proceed: null,
+  cancel: null,
+};
+export const CONFIRM_MODAL_INIT = 'CONFIRM_MODAL_INIT';
+export const CONFIRM_MODAL_CLOSE = 'CONFIRM_MODAL_CLOSE';
