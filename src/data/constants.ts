8c4647b8ed040a8561c0459b502db578f03cf79e
deftonjke <defton@mail.ru>
2022-05-13 04:00:43 +0400

    refactor: move to another file

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -2,15 +2,3 @@ export const API_URL = 'https://rs-team-34.herokuapp.com';
 
 export const LANG_RU = 'RU';
 export const LANG_EN = 'EN';
-
-export const SET_BOARDS = 'SET_BOARDS';
-
-export const CONFIRM_MODAL_DEFAULT = {
-  question: '',
-  isOpen: false,
-  proceed: null,
-  cancel: null,
-};
-
-export const CONFIRM_MODAL_INIT = 'CONFIRM_MODAL_INIT';
-export const CONFIRM_MODAL_CLOSE = 'CONFIRM_MODAL_CLOSE';
