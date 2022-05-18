74aa6d845a9c847f42ebd6054555c0e76150cdc9
deftonjke <defton@mail.ru>
2022-05-18 06:06:15 +0400

    feat: add column component

---
+++ src/components/Column/Column.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import { TaskResponse } from '../../data/interfacesV';
+import CreateTaskModal from '../CreateTaskModal/CreateTaskModal';
+
+function Column({
+  columnId,
+  title,
+  boardId,
+  tasks,
+  handleDelete,
+  loadBoard,
+}: {
+  columnId: string;
+  title: string;
+  boardId: string;
+  tasks: TaskResponse[];
+  handleDelete: () => void;
+  loadBoard: () => Promise<void>;
+}) {
+  const [isTaskCreateOpen, setIsTaskCreateOpen] = useState(false);
+  const newTaskOrder = tasks.length ? [...tasks].sort((a, b) => b.order - a.order)[0].order + 1 : 1;
+
+  const tasksArray = tasks.map((task) => (
+    <div key={task.id} className="list__task">
+      {task.done ? (
+        <i className="fa-regular fa-square-check"> </i>
+      ) : (
+        <i className="fa-regular fa-square"> </i>
+      )}
+      <span style={{ marginLeft: '5px' }}>{task.title}</span>
+    </div>
+  ));
+
+  return (
+    <div className="list-wrapper">
+      <div className="list">
+        <div className="list__header">
+          <h3 className="list__title">{title}</h3>
+          <button className="list__delete-btn" type="button" onClick={handleDelete}>
+            <i className="fa-solid fa-xmark"> </i>
+          </button>
+        </div>
+        <div className="list__tasks">{tasksArray}</div>
+        <div className="add-task-container">
+          <button className="add-task" type="button" onClick={() => setIsTaskCreateOpen(true)}>
+            <i className="fa-solid fa-plus"> </i>
+            Add a task
+          </button>
+        </div>
+      </div>
+      {isTaskCreateOpen && (
+        <CreateTaskModal
+          columnId={columnId}
+          boardId={boardId}
+          order={newTaskOrder}
+          loadBoard={loadBoard}
+          setIsTaskCreateOpen={setIsTaskCreateOpen}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Column;
