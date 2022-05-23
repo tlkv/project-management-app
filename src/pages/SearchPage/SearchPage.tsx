67a73c1c7ea59a220c27e3b8e6f239264ba93afb
A0009 <aaa144855@gmail.com>
2022-05-23 03:18:54 +0300

    feat: update search page, remove unused component

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,7 +1,4 @@
-/* eslint-disable react/jsx-no-bind */
-/* eslint-disable react/jsx-props-no-spreading */
 import React, { useContext, useEffect, useState } from 'react';
-import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
 import { useNavigate } from 'react-router-dom';
 import './SearchPage.scss';
 import { AppContext } from '../../App';
@@ -9,38 +6,10 @@ import getAllTasks from '../../api/getAllTasks';
 import { TaskResponse } from '../../data/interfacesV';
 import SearchTaskInfo from '../../components/SearchTaskInfo/SearchTaskInfo';
 
-const mock: { name: string; id: string }[] = [
-  {
-    name: '1',
-    id: '1',
-  },
-  {
-    name: '2',
-    id: '2',
-  },
-  {
-    name: '3',
-    id: '3',
-  },
-  {
-    name: '4',
-    id: '4',
-  },
-];
-
 export default function SearchPage() {
   const { isAuth, logoutUser } = useContext(AppContext);
   const navigate = useNavigate();
   const [tasks, setTasks] = useState<TaskResponse[]>([]);
-
-  const [characters, updateCharacters] = useState(mock);
-  function handleOnDragEnd(results: DropResult) {
-    if (!results.destination) return;
-    const items = Array.from(characters);
-    const [reorderedItem] = items.splice(results.source.index, 1);
-    items.splice(results.destination.index, 0, reorderedItem);
-    updateCharacters(items);
-  }
 
   useEffect(() => {
     if (!isAuth && !localStorage.getItem('pmapp34-token')) {
@@ -79,31 +48,6 @@ export default function SearchPage() {
           />
         ))}
       </div>
-      <DragDropContext onDragEnd={handleOnDragEnd}>
-        <Droppable droppableId="characters" direction="horizontal">
-          {(provided) => (
-            <div className="drop-container" {...provided.droppableProps} ref={provided.innerRef}>
-              {characters.map(({ name, id }, index) => {
-                return (
-                  <Draggable key={id} draggableId={id} index={index}>
-                    {(providedColumn) => (
-                      <div
-                        className="drop-item"
-                        {...providedColumn.draggableProps}
-                        {...providedColumn.dragHandleProps}
-                        ref={providedColumn.innerRef}
-                      >
-                        {name}
-                      </div>
-                    )}
-                  </Draggable>
-                );
-              })}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
     </div>
   );
 }
